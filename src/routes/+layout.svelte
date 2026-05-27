2adceeb4268d8c9afd84af9ecd756055da8578e2
andressb98 <andressolisbautista15@gmail.com>
2026-05-27 12:58:30 -0600

    feat: eliminar favicon y redirigir al registro desde el servidor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import './layout.css';
-    import favicon from '$lib/assets/favicon.svg';
     import { page } from '$app/stores';
     import { currentUser } from '$lib/store';
     // Ya no necesitas onMount para esta sincronización básica
@@ -13,8 +12,5 @@
     });
 </script>
 
-<svelte:head>
-    <link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
